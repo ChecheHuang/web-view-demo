53385066825cc9ea436a2bc7b1a08f1aa54227f8
Carl Huang <carl.huang@pstarc.com>
2024-07-26 17:50:51 +0800

    master(master): 123
    需求
    
    123
    
    調整
    
    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
 import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
-
+import { createRef, useState } from "react";
+declare const JsTest: {
+  showToast: (message: string) => void;
+};
+function calledFromAndroid(msg: string) {
+  alert("calledFromAndroid");
+  document!.getElementById("mytext")!.innerHTML = msg;
+}
 export default function Home() {
-  const [value, setValue] = useState("");
+  const inputRef = createRef<HTMLInputElement>();
+  const [text, setText] = useState("這是原本React裡面的字");
 
   const sendDataToApp = () => {
-    console.log(value);
-    setValue("");
+    if (!inputRef.current) return;
+    showAndroidToast(inputRef.current.value);
+    inputRef.current.value = "";
+    inputRef.current.focus();
   };
+  // function calledFromAndroid(msg: string) {
+  //   setText(msg);
+  // }
+
+  function showAndroidToast(toast: string) {
+    console.log("傳給APP", toast);
+    JsTest.showToast(toast);
+  }
 
   return (
     <div className=" w-full h-[100vh]  flex flex-col justify-center items-center ">
       <div className="w-100 flex flex-col gap-2">
+        <Button onClick={() => calledFromAndroid("給App用的方法")}>
+          測試給App的方法
+        </Button>
         <div className="flex gap-4">
-          <Input
-            className="w-60 "
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
+          <Input className="w-60 " ref={inputRef} />
           <Button onClick={sendDataToApp}>傳送資料給App</Button>
         </div>
         <Alert variant="default">
           <AlertTitle>給App修改的字</AlertTitle>
-          <AlertDescription>這是原本React裡面的字</AlertDescription>
+          <AlertDescription id="mytext">{text}</AlertDescription>
         </Alert>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import terser from "@rollup/plugin-terser";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     port: 5173,
     host: "0.0.0.0",
@@ -13,5 +15,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    minify: false,
   },
 });
